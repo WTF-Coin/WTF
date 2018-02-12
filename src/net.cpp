688aa4e8a0a8a6cbb3a7adab33d3337427b059f9
pacificao <nathan@pacificao.com>
2018-02-12 07:00:32 +0000

    rc1.1

--- src/net.cpp
+++ src/net.cpp
@@ -1192,11 +1192,11 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {NULL, NULL}
+    {wtf.pacificao.com, wtf.pacificao.com}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {NULL, NULL}
+    {wtf.pacificao.com, wtf.pacificao.com}
 };
 
 void ThreadDNSAddressSeed()
